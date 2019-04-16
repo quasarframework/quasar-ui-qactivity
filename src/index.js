bee26b49a2dd120d8ce9301cd72c50677cf5b4e4
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-04-16 17:36:10 +0300

    feat: upgrade to latest @quasar/app specs

--- src/index.js
+++ src/index.js
@@ -6,33 +6,21 @@
  */
 
 const extendQuasarConf = function (api, conf) {
-  // for brevity
-  let boot = conf.boot
-
   // make sure qactivity boot file is registered
-  const bootFile = '~@quasar/quasar-app-extension-qactivity/src/boot/qactivity.js'
-  if (!boot.includes(bootFile)) {
-    boot.push(bootFile)
-    console.log(` App Extension (qactivity) Info: 'Adding qactivity boot reference to your quasar.conf.js'`)
-  }
+  conf.boot.push('~@quasar/quasar-app-extension-qactivity/src/boot/qactivity.js')
+  console.log(` App Extension (qactivity) Info: 'Adding qactivity boot reference to your quasar.conf.js'`)
 
-  // make sure boot file transpiles
+  // make sure boot file & component transpiles
   conf.build.transpileDependencies.push(/quasar-app-extension-qactivity[\\/]src/)
 
-  // for brevity
-  let css = conf.css
-
   // make sure qactivity css goes through webpack to avoid ssr issues
-  const cssFile = '~@quasar/quasar-app-extension-qactivity/src/component/activity.styl'
-  if (!css.includes(cssFile)) {
-    css.push(cssFile)
-    console.log(` App Extension (qactivity) Info: 'Adding activity.styl css reference to your quasar.conf.js'`)
-  }
+  conf.css.push('~@quasar/quasar-app-extension-qactivity/src/component/activity.styl')
+  console.log(` App Extension (qactivity) Info: 'Adding activity.styl css reference to your quasar.conf.js'`)
 }
 
 module.exports = function (api) {
   // quasar compatibility check
-  api.compatibleWithQuasarApp('^1.0.0-beta.17')
+  api.compatibleWith('@quasar/app', '^1.0.0-beta.18')
 
   // register JSON api
   api.registerDescribeApi('QActivity', './component/QActivity.json')
