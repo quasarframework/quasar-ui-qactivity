e5510843adcc88842fff3e6af522bfa965a56158
Christian Behon <christian.behon@knusperleicht.at>
2021-07-09 19:42:08 +0200

    feat(qactivity): rename everything to *.old and introduce new ext. folder structure
    * Add app-extension folder
    * Add ui folder for component/directive
    * Add/Update README and CHANGELOG
    * Update some files and dependencies
    * Fix wrong github project urls
    * Introduce vetur for mono repo

---
+++ app-extension/src/boot/register.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import VuePlugin from '@quasar/quasar-ui-qactivity/QActivity'
+import VuePlugin from '@quasar/quasar-ui-qactivity/QActivityItem'
+
+Vue.use('q-activity', QActivity)
+Vue.use('q-activity-item', QActivityItem)
+
---
+++ ui/src/components/QActivityItem.js
@@ -0,0 +1,4 @@
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'QActivityItem'
+})
