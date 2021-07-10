807c92a09df253abfe05a991604e8d43de6c38f4
Christian Behon <christian.behon@knusperleicht.at>
2021-07-10 16:23:11 +0200

    fix(imports): fix wrong imports in register js

--- app-extension/src/boot/register.js
+++ app-extension/src/boot/register.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import VuePlugin from '@quasar/quasar-ui-qactivity/QActivity'
-import VuePlugin from '@quasar/quasar-ui-qactivity/QActivityItem'
+import QActivity from '@quasar/quasar-ui-qactivity/QActivity'
+import QActivityItem from '@quasar/quasar-ui-qactivity/QActivityItem'
 
 Vue.use('q-activity', QActivity)
 Vue.use('q-activity-item', QActivityItem)
