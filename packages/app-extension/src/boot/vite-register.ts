8d6070a33341fa691aaf7f6811eaf2aaf41c7d9f
Jeff <galbraith64@gmail.com>
2026-07-21 15:21:28 -0600

    chore: update dependencies and QPress templates

--- packages/app-extension/src/boot/vite-register.ts
+++ packages/app-extension/src/boot/vite-register.ts
@@ -1,5 +1,7 @@
 import { defineBoot } from '#q-app'
 import VuePlugin from '@quasar/quasar-ui-qactivity'
+// @ts-expect-error SCSS side-effect imports do not have type declarations
+import '@quasar/quasar-ui-qactivity/src/index.scss'
 
 export default defineBoot(({ app }) => {
   app.use(VuePlugin)
--- packages/app-extension/src/index.ts
+++ packages/app-extension/src/index.ts
@@ -19,6 +19,5 @@ export default defineIndexScript((api) => {
 
   api.extendQuasarConf(() => ({
     boot: ['~@quasar/quasar-app-extension-qactivity/dist/boot/vite-register.js'],
-    css: ['~@quasar/quasar-ui-qactivity/src/index.scss'],
   }))
 })
